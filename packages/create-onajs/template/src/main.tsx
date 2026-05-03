2cfd0c269cd1b0baf3615c334e853f52705da604
Adavize Hassan <adavize302@gmail.com>
2026-05-03 08:20:17 +0100

    chore: cleanup template

--- packages/create-onajs/template/src/main.tsx
+++ packages/create-onajs/template/src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { Suspense } from 'react'
+import { OnaRoutes } from '@ize-302/onajs/routes'
+import { routes } from 'virtual:ona-manifest'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Suspense fallback={null}>
+        <OnaRoutes routes={routes} />
+      </Suspense>
+    </BrowserRouter>
   </StrictMode>
 )
