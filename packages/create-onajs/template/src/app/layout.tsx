6c80e62b9366ba154a382a8a5c0af76506a594d7
Adavize Hassan <adavize302@gmail.com>
2026-05-03 08:26:44 +0100

    chore: remove non-existing routes from template layout

--- packages/create-onajs/template/src/app/layout.tsx
+++ packages/create-onajs/template/src/app/layout.tsx
@@ -1,18 +1,14 @@
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, Link } from "react-router-dom";
 
 export default function RootLayout() {
   return (
     <div>
       <nav>
         <Link to="/">Home</Link>
-        {' · '}
-        <Link to="/about">About</Link>
-        {' · '}
-        <Link to="/contact">Contact</Link>
-        {' · '}
+        {" · "}
         <Link to="/blog">Blog</Link>
       </nav>
       <Outlet />
     </div>
-  )
+  );
 }
